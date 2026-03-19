a83820dba8dcd0b031fbbd34ac40c16cbd68d1bb
mireeen <mailharin.148443@e.unavarra.es>
2026-03-19 16:11:07 +0100

    Filtro peliculas arreglado

--- src/components/peliculafiltro.jsx
+++ src/components/peliculafiltro.jsx
@@ -16,8 +16,13 @@ function PeliculasFiltro(props) {
                     <Form.Label>Selecciona el genero:</Form.Label>
                     <Form.Select onChange={genderHandler} value={props.genero}>
                         <option value=''>Ver todos</option>
-                        <option value='drama'>drama</option>
-                        <option value='risa'>risa</option>
+                        <option value='Drama'>Drama</option>
+                        <option value='Comedia'>Comedia</option>
+                        <option value='Acción'>Acción</option>
+                        <option value='Terror'>Terror</option>
+                        <option value='Ciencia ficción'>Ciencia ficción</option>
+                        <option value='Animación'>Animación</option>
+                        <option value='Thriller'>Thriller</option>
                     </Form.Select>
                 </Col>
             </Row>
